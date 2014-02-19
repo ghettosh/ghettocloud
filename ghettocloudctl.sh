049a3afbf0411822516537bccfa709ed5d5231c0
Niko router <og@ghetto.sh>
2014-02-19 02:01:07 -0700

    accounting pieces

---
+++ ghettocloudctl.sh
@@ -0,0 +1,96 @@
+#!/usr/bin/env bash
+
+###############################################################################
+#
+# Functions
+#
+
+function print_blue(){ printf "$(tput setf 1)$@$(tput sgr0)"; }
+function print_green(){ printf "$(tput setf 2)$@$(tput sgr0)"; }
+function print_blue(){ printf "$(tput setf 3)$@$(tput sgr0)"; }
+function print_red(){ printf "$(tput setf 4)$@$(tput sgr0)"; }
+function print_pink(){ printf "$(tput setf 5)$@$(tput sgr0)"; }
+function print_yellow(){ printf "$(tput setf 6)$@$(tput sgr0)"; }
+function print_white(){ printf "$(tput setf 7)$@$(tput sgr0)"; }
+
+function usage(){
+    print_red "Usage:\n"
+    echo " $0 <-c command> "
+    print_red "Commands are any one of the following:\n"
+    echo "  list-vms"
+    echo "  list-hypervisors"
+    echo "  show-checkin <vm name>"
+    echo "  show-bootlog <vm name>"
+    exit 1
+}
+
+function list-vms(){
+    if [[ $DEBUG ]]; then
+        print_yellow "DEBUG: Command -> $command\n"
+    fi
+    ${SQLITE3} -header -column ${DBFILE} "select * from vms;"
+}
+
+#
+# End Functions
+#
+
+###############################################################################
+#
+# Variables
+#
+
+ROADSIGN="http://ghetto.sh/roadsign.txt"
+API_HEAD="$(ftp -Vo- -r 5 ${ROADSIGN} 2>/dev/null)"
+OS="$(uname)"
+
+#
+# End Variables
+#
+
+###############################################################################
+#
+# Checks and getopt
+#
+
+if [[ -z ${API_HEAD} ]]; then
+    print_red "FATAL: Could not contact the API head\n"
+    exit 1
+fi
+
+if [[ $DEBUG ]]; then
+    print_yellow "DEBUG: API Head -> ${API_HEAD}\n"
+fi
+
+if [[ "${OS}" != "OpenBSD" ]]; then                                          
+    print_red "FATAL: This script is only meant to run on OpenBSD\n"            
+    exit 1                                                                      
+fi 
+
+while getopts ":c:d" o; do
+    case "${o}" in
+        c)
+            command=${OPTARG}
+            ;;
+        d)  DEBUG=1;;
+        *)
+            usage
+            ;;
+    esac
+done
+shift $((OPTIND-1))
+
+if [ -z "${command}" ]; then
+    usage
+fi
+
+
+###############################################################################
+#
+# main()
+#
+
+case ${command} in
+    list-vms)   list-vms;;
+    *)          print_red "Unrecognized command: ${command}\n"; exit 1;;
+esac
---
+++ initialize-db.sh
@@ -0,0 +1,37 @@
+#!/usr/bin/env bash
+
+function usage(){
+    echo "Usage: $0 <db file>"
+    exit 1
+}
+
+SQLITE3=`which sqlite3 2>/dev/null`
+SQLITE3=${SQLITE3:?FATAL: no sqlite3 found}
+
+DBFILE=$1
+DBFILE=${DBFILE:?$(usage)}
+
+if [[ ! ${DBFILE} =~ *.db ]]; then DBFILE=${DBFILE}.db; fi
+
+if [[ ! -r ${DBFILE} ]]; then 
+    echo "==> Database file: ${DBFILE} not found, creating one"
+else
+    echo "==> Database file ${DBFILE} found, backing it up and reinitializing"
+    BACKUP=$DBFILE-backup-$(date +%s)
+    mv $DBFILE $BACKUP
+    echo "==> moved $DBFILE to $BACKUP"
+fi
+
+echo "==> reinitializing database at ${DBFILE}"
+
+${SQLITE3} ${DBFILE} "create table vms (id INTEGER PRIMARY KEY,
+                  hostname TEXT, 
+                  realname TEXT,
+                  macaddr TEXT, 
+                  rootpw TEXT, 
+                  hypervisor TEXT,
+                  creationdate TEXT)"
+
+echo "==> initialized db"
+echo "==> schema:"
+${SQLITE3} ${DBFILE} ".schema"
