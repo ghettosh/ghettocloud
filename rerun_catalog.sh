29dd9b39674acaf24ada2d35c594a7388ba6333d
Niko router <og@ghetto.sh>
2014-02-27 16:34:23 -0700

    adding a catalog running script

---
+++ rerun_catalog.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+
+# 
+# This script reruns a cdist catalog on all existing hypervisors.
+#
+
+CDIST_DIR=/data/cdist/
+CDIST_BIN=${CDIST_DIR}/bin/cdist
+DBFILE=/data/serve/ghettoapi/cgi/ghetto.db
+SQLITE3=`which sqlite3`
+SQL="SELECT ip FROM vms;"
+
+for VM in $( ${SQLITE3} ${DBFILE} "${SQL}" ); do
+  ${CDIST_BIN} -v config $VM &
+done
+
+wait
